5fd9fd089d1d2b7d2c14abbaa637ef1dbaf82950
Krzysztof Szewczyk <marekszuwarek1958@gmail.com>
2018-01-06 12:48:50 +0100

    Setup basic form and aboutbox code.

--- src/medit/Start.java
+++ src/medit/Start.java
@@ -1,10 +1,36 @@
 package medit;
 
+import java.awt.EventQueue;
+
+import javax.swing.UIManager;
+
+/**
+ * Main class for MEdit project.
+ * This thing is launching MainFrame window.
+ * @author Krzysztof Szewczyk
+ */
+
 public class Start {
 
+	/**
+	 * Main function of Start class.
+	 * Creating MainFrame object.
+	 * @param args
+	 * @see medit.MainFrame
+	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
