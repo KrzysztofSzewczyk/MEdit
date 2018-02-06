af36a62fa3f50293dd127501a67263e2f36166b5
Krzysztof Szewczyk <marekszuwarek1958@gmail.com>
2018-02-06 12:19:33 +0100

    Fix mistake in package-info.java

--- src/main/java/medit/API/package-info.java
+++ src/main/java/medit/API/package-info.java
@@ -1,6 +1,6 @@
 
 /**
- * This class was made to help B++ programmers <s>to cure them from cancer</s>
+ * This package was made to help B++ programmers <s>to cure them from cancer</s>
  * 
  * @author Krzysztof Szewczyk
  *
