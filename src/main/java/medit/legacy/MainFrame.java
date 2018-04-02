91b7c71ab5b072c52183b3d2fcced9d9714c2ad3
Krzysztof Szewczyk <marekszuwarek1958@gmail.com>
2018-04-02 14:13:04 +0200

    Added import for JInternalFrame

--- src/main/java/medit/legacy/MainFrame.java
+++ src/main/java/medit/legacy/MainFrame.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import javax.swing.JDesktopPane;
 import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
